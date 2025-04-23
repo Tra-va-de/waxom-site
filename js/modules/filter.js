7ab84a9aec9ec33241b9601274158b2aaaecd14c
Tra-va-de <distraction.mansion@gmail.com>
2025-04-23 18:10:57 +0000

    Copy files to build branch

--- js/modules/filter.js
+++ js/modules/filter.js
@@ -1,1 +1,35 @@
-let initFilter=()=>{document.querySelectorAll(".projects__category-btn").forEach(t=>{t.addEventListener("click",()=>filterCards(t))})};function filterCards(t){document.querySelector(".projects__category-btn--active").classList.remove("projects__category-btn--active"),t.classList.add("projects__category-btn--active");let e=t.getAttribute("data-category"),r=document.querySelector(".projects__cards");r.classList.add("hide"),r.addEventListener("transitionend",()=>{r.setAttribute("data-category",e),r.classList.remove("hide")},{once:!0})}export{initFilter};
+// Сортировка по категориям
+export const initFilter = () => {
+    const filterButtons = document.querySelectorAll('.projects__category-btn');
+    
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => filterCards(button));
+    });
+};
+
+function filterCards(button) {
+    // Убираем активный класс у текущей кнопки
+    document.querySelector('.projects__category-btn--active').classList.remove('projects__category-btn--active');
+
+    // Добавляем активный класс к текущей кнопке
+    button.classList.add('projects__category-btn--active');
+
+    // Получаем категорию текущей кнопки
+    const category = button.getAttribute('data-category');
+
+    // Получаем контейнер с карточками
+    const cardsContainer = document.querySelector('.projects__cards');
+
+    // Скрываем контейнер с карточками
+    cardsContainer.classList.add('hide');
+
+    // После завершения скрытия прячем карточки
+    cardsContainer.addEventListener('transitionend', () => {
+        // Добавляем категорию отображаемых карточек 
+        // в атрибут контейнера
+        cardsContainer.setAttribute('data-category', category);
+
+        // Отображаем контейнер с карточками
+        cardsContainer.classList.remove('hide');
+    }, { once: true });
+} 
--- js/modules/swiper.js
+++ js/modules/swiper.js
@@ -1,1 +1,37 @@
-let initHeaderSwiper=()=>{new Swiper(".header__content",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})},initPostsSwiper=()=>{new Swiper(".posts__swiper",{direction:"horizontal",loop:!0,slidesPerView:3,spaceBetween:30,navigation:{nextEl:".posts__button--next",prevEl:".posts__button--prev"}})};export{initHeaderSwiper,initPostsSwiper};
+// Настройка слайдера первой секции
+export const initHeaderSwiper = () => {
+    const headerSwiper = new Swiper('.header__content', {
+        // Основные параметры
+        direction: 'horizontal',
+        loop: true,
+
+        // Пагинация
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // Стрелки
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+};
+
+// Свайпер секции posts
+export const initPostsSwiper = () => {
+    const postsSwiper = new Swiper('.posts__swiper', {
+        // Основные параметры
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+
+        // Стрелки
+        navigation: {
+            nextEl: '.posts__button--next',
+            prevEl: '.posts__button--prev',
+        },
+    });
+}; 
